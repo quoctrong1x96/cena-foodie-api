2ef300cedcf1e927ee0c0fc192de69db2b2830ac
TRONGBQ <quoctrong1x96@gmail.com>
2022-05-08 22:19:49 +0700

    update to production

--- src/Controller/StoreController.js
+++ src/Controller/StoreController.js
@@ -3,6 +3,26 @@ import { response } from 'express';
 import pool from '../Database/mysql';
 
 
+export const getStoreNameById = async (req, res = response) => {
+
+    try {
+        const stores = await pool.query(`SELECT store_name,address  FROM stores where id = ?`, req.params.id);
+
+        res.json({
+            resp: true,
+            msg : 'Store id = '+ req.params.storeId,
+            stores_name: stores 
+        });
+
+        
+    } catch (e) {
+        return res.status(500).json({
+            resp: false,
+            msg : e
+        });
+    }
+
+}
 export const getStoresList = async (req, res = response) => {
 
     try {
--- src/Router/Store.routes.js
+++ src/Router/Store.routes.js
@@ -6,5 +6,6 @@ const router = Router();
 
 
 router.get('/get-all-store-by-page', verifyToken, store.getStoresList);
+router.get('/store/:id/name', verifyToken, store.getStoreNameById);
 
 export default router;
