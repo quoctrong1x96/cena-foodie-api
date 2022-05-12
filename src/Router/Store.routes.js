a7c097c78abe1cb5f9ba53a12f88fdeffc767211
TRONGBQ <quoctrong1x96@gmail.com>
2022-05-12 07:54:23 +0700

    chỉnh sửa thêm nội dung api

--- src/Router/Store.routes.js
+++ src/Router/Store.routes.js
@@ -7,5 +7,6 @@ const router = Router();
 
 router.get('/get-all-store-by-page', verifyToken, store.getStoresList);
 router.get('/store/:id/name', verifyToken, store.getStoreNameById);
+router.get('/stores/pages', verifyToken, store.getStoresPerPage);
 
 export default router;
